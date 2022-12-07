217065d1e11b23d94077e9765fe6bcc2327ba68f
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-07 15:27:13 -0300

    Create eh_muieh.c

---
+++ eh_muieh.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+int main(){
+int idade;
+char nome, sexo, gen;
+printf("Qual é seu sexo, nome e idade?");
+scanf("%s", sexo);
+scanf("%s", nome);
+scanf("%d", idade);
+gen = "mulher"
+if int strcmp(char *nome, char *gen)=1
+  printf("eh mulher";
+}
+// outro
+# include <stdio.h>
+int main(){
+    int idade;
+    char nome[10], sexo[10], gen[10];
+    printf("Qual é seu sexo, nome e idade?");
+    scanf("%s", &sexo);
+    scanf("%s", &nome);
+    scanf("%d", &idade);
+    gen = &"mulher";
+    if strcmp(char *nome, char *gen)=1;
+        printf("eh mulher");
+}
+         //no sei mama mia o char nao tá pegando ou algo assim
