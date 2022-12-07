4333049ad3cb8f1fd8df6ba29e82e20d97439113
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-07 15:02:51 -0300

    Create novo_salario.c

---
+++ novo_salario.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+int salario, novo_salario;
+  printf("Qual eh seu salario? ");
+  scanf("%d", &salario);
+  novo_salario = (salario*0.1)+ salario;
+  printf("Seu novo salario eh %d", novo_salario);
+}
