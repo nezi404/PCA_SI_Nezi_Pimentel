74b19883df8a81888a6b55b9d8c145fd7dc72a60
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-09 19:46:23 -0300

    Update Exercício_2.c

--- Exercício_2.c
+++ Exercício_2.c
@@ -3,7 +3,7 @@
 
 int main(){
     int idade;
-    char nome[14], sexo[14], gen[14] = "mulher";
+    char nome[14], sexo[14], gen[14] = "mulher", out[10] = "homem", genm[10] = "M", genh[10] = "H";
     printf("Qual é seu sexo?");
     scanf("%s", sexo);
     printf("Qual é seu nome?");
@@ -11,12 +11,15 @@ int main(){
     printf("Qual é sua idade?");
     scanf("%d", &idade);
     
-    if (strcmp(sexo, gen)==0) {
+    if (strcmp(sexo, gen)==0 || strcmp(genm, sexo)==0) {
         printf("é mulher");
     }
-    else{
+    else
+    if (strcmp(sexo, out)==0 || strcmp(sexo, genh)==0) {
         printf("não é mulher");
     } 
-    
+    else {
+        printf("Caractere(s) inválido(s), somente H, M, homem ou mulher!");
+    }
     return 0;
 }
