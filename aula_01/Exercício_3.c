68464e76132a12d1bb15e853d5f4a39f9a7c8dba
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-11 23:10:51 -0300

    Update Exercício_3.c

--- aula_01/Exercício_3.c
+++ aula_01/Exercício_3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-int salario, novo_salario;
-  printf("Qual eh seu salario? ");
-  scanf("%d", &salario);
+float salario, novo_salario;
+  printf("Qual eh seu salario? R$");
+  scanf("%f", &salario);
   novo_salario = (salario*0.1)+ salario;
-  printf("Seu novo salario eh %d", novo_salario);
+  printf("Seu novo salario eh R$%0.2f", novo_salario);
 }
