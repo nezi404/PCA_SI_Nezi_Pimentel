a233c002fb9f9db47c9b556e9ba017d10e017243
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-15 17:12:14 -0400

    Create 20221215_003.c

---
+++ atividade_2/20221215_003.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int prim, resul;
+    printf("Insira o numero para convercao em cm: ");
+    scanf("%d", &prim);
+    resul = prim*100;
+    printf("O valor da convercao eh %d", resul);
+}
