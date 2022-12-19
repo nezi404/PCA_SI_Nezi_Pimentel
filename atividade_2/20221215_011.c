3e65b1bad2b9fdae62af065d3455069adbe26c8e
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-19 11:49:45 -0300

    Update 20221215_011.c

--- atividade_2/20221215_011.c
+++ atividade_2/20221215_011.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/* 
+20221215_011.  Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda,
+8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
+salário bruto.
+Quanto pagou ao INSS.
+quanto pagou ao sindicato.
+o salário líquido (Salário Líquido = Salário Bruto - Descontos)
+*/
+
 int main() {
     float sal_hor, hor, sal, ir, inss, sin, salliq; 
     printf("Quanto vc ganha por hora?");
