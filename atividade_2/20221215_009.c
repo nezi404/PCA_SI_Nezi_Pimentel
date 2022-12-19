f9ae295fe395c81d1f7c6ac9a01232c8cee5c650
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-19 10:51:02 -0300

    Create 20221215_009.c

---
+++ atividade_2/20221215_009.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num1, num2, resul3;
+    float num3, resul1, resul2;
+    printf("Primeiro número inteiro: ");
+    scanf("%d", &num1);
+    printf("Segundo número inteiro: ");
+    scanf("%d", &num2);
+    printf("Terceiro número: ");
+    scanf("%f", &num3);
+    resul1 = (2 * num1) * (num2/2);
+    resul2 = (3 * num1 + num3); 
+    resul3 = pow(num3, 3);
+    printf("O produto do dobro do primeiro com metade do segundo: %0.2f\nA soma do triplo do primeiro com o terceiro %0.2f\nO terceiro elevado ao cubo: %d",resul1, resul2, resul3);
+}
