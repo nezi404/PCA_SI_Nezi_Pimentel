32d2623d32c719615aa024a722c0edfee4297ad4
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-19 11:48:48 -0300

    Update 20221215_010.c

--- atividade_2/20221215_010.c
+++ atividade_2/20221215_010.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
+/*
+20221215_010. Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo
+que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+*/
+
 int main() {
     float altura, peso_i;
     printf("Qual sua altura em metros? ");
