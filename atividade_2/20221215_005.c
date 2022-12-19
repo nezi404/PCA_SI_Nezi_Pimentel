ba1adf114a765799874fb1281cb3f1182a6f5aa0
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-19 11:21:02 -0300

    Update 20221215_005.c

--- atividade_2/20221215_005.c
+++ atividade_2/20221215_005.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+//20221215_005. Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+
 int main (void){
     float lado, area_dupla;
     printf("Qual é o tamanho do lado? ");
