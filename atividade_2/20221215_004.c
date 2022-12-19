c5b4d01bf6427433dfebdb8a5f519590910bdd9e
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-19 11:04:02 -0300

    Update 20221215_004.c

--- atividade_2/20221215_004.c
+++ atividade_2/20221215_004.c
@@ -3,7 +3,7 @@
 
 int main (void){
     float raio, pi = 3.1415, area;
-    printf("Qual é o raio do círculo?");
+    printf("Qual é o raio do círculo? ");
     scanf("%f", &raio);
     area = pow(raio, 2) * pi;
     printf("A área do círculo é %0.2f", area);
