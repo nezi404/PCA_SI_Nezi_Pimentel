31c5517a6623da53ba2d78d3f39c4218c0a71e23
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-19 11:40:30 -0300

    Update 20221215_006.c

--- atividade_2/20221215_006.c
+++ atividade_2/20221215_006.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+/* 20221215_006.  Faça um Programa que pergunte quanto você ganha por hora e
+o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+*/
+
 int main() {
-    float sal_hor, hor, sal, ir, inss, sin, salliq; 
+    float sal_hor, hor, sal; 
     printf("Quanto vc ganha por hora?");
     scanf("%f", &sal_hor);
-    printf("Quantas horas você trabalha?");
+    printf("Quantas horas você trabalha no mês?");
     scanf("%f", &hor);
     sal = hor * sal_hor ;
     
