deefe7a6c36529a65ddb93cf7ff856e0fffa8291
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-19 11:41:40 -0300

    Update 20221215_007.c

--- atividade_2/20221215_007.c
+++ atividade_2/20221215_007.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*
+20221215_007.  Faça um Programa que peça a temperatura em graus Farenheit,
+transforme e mostre a temperatura em graus Celsius. C = (5 * (F-32) / 9).
+*/
+
 int main() {
     float fire, cel;
     printf("Temperatura em Fareheit: ");
