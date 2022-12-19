83c8ea302a4cbe4efa8d6603ad99531c3855a9da
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-19 00:18:19 -0300

    Update 20221215_011.c

--- atividade_2/20221215_011.c
+++ atividade_2/20221215_011.c
@@ -3,17 +3,13 @@
 int main() {
     float sal_hor, hor, sal, ir, inss, sin, salliq; 
     printf("Quanto vc ganha por hora?");
-    scanf("%", &sal_hor);
-    printf("Quanto vc ganha por hora?");
-    scanf("%", &hor);
-    sal = hor * sal_hor;
-    sin = sal * 0.5;
+    scanf("%f", &sal_hor);
+    printf("Quantas horas você trabalha?");
+    scanf("%f", &hor);
+    sal = hor * sal_hor * 30;
+    sin = sal * 0.05;
 	ir = sal * 0.11;
-    inss = sal * 0.8;
+    inss = sal * 0.08;
     salliq = sal - ir -inss -sin;
-    printf("SALARIO BRUTO:  R$%0.2f\nPAGO AO INSS: R$%0.2f\nVALOR PAGO AO SINDICADO: R$%0.2f\nSALARIO LIQUIDO : R$%0.2f\n", (sal, inss, sin,salliq));
+    printf("SALARIO BRUTO:  R$%0.2f\nPAGO AO INSS: R$%0.2f\nVALOR PAGO AO SINDICADO: R$%0.2f\nSALARIO LIQUIDO : R$%0.2f\n", sal, inss, sin,salliq);
 }
-
-
-
-// search for answer while in ooping
