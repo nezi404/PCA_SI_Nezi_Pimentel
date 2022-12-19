c18afcbd47b8964476f93fd128a4d2b098e27e9c
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-19 11:02:34 -0300

    Update 20221215_008.py

--- atividade_2/20221215_008.py
+++ atividade_2/20221215_008.py
@@ -5,5 +5,5 @@ int main() {
     printf("Temperatura em celsius: ");
     scanf("%f", &cel);
     fire = ((cel)*9/5) + 32;
-    printf("%0.1f graus são %0.1f graus Farenheit", cel, fire);
+    printf("%0.1f graus celsius são %0.1f graus Farenheit", cel, fire);
 }
