6a0e7072602e596cc0877ca43721ce2ef5462cf4
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-20 17:26:45 -0400

    Create loopoing_required.c

---
+++ aula_3/loopoing_required.c
@@ -0,0 +1,15 @@
+ /* aula de condicionais
+ formas : if e else 
+ switch e case, so funciona com inteiros oe char, nao funciona com floats 
+ comando ternario, o que usa ponto sw exclamacao e um linha
+ 
+ while mm coisa que while do python, mas usa parenteses
+ 
+ do-while
+ executa o comndo antes de avaliar a expressao
+ 
+ for inicializacao, expressao,  booleana e e incremento
+ declara a valiavel controle no inicio do programa
+ cont
+ */
+fazer u que ler oque tem o valor e eo ler mais o que tem ovalor
