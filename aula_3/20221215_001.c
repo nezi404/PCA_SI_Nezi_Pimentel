d1989903f7f072780f5863ca633182acd841adb7
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-20 17:28:09 -0400

    Create 20221215_001.c

---
+++ aula_3/20221215_001.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int cont = 0, num = 0, num1 ;
+  while (cont < 5){
+      cont = cont + 1;
+      printf("Numeros para soma: ");
+      scanf("%d", &num1);
+      num = num1 + num;}
+printf("a soma: %d", num);
+  
+}
+
+// twm que ter so uma variavel
