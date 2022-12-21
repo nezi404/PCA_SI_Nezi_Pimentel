bb04c9760de6f4cd067ed40bb2f9adc04c95c4bc
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-21 00:15:14 -0400

    Create 202222220006.c

---
+++ aula_3/202222220006.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+  int termo1=1, termo2=1, resul;
+  while (termo1 < 10){
+        while (termo2 < 10){
+            resul = termo1 + termo2;
+            printf("%d + % d = %d\n", termo1, termo2, resul);
+                termo2 = termo2 + 1;}
+        termo1 = termo1 + 1;}
+       
+     
+  
+}
+
