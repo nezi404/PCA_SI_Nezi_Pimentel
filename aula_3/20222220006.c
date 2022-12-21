b2ad53c0f22a0bd9395dd6898ca185cd81dc03da
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-21 00:21:08 -0400

    Update and rename 202222220006.c to 20222220006.c

--- aula_3/20222220006.c
+++ aula_3/20222220006.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 int main() {
@@ -8,9 +7,15 @@ int main() {
             resul = termo1 + termo2;
             printf("%d + % d = %d\n", termo1, termo2, resul);
                 termo2 = termo2 + 1;}
-        termo1 = termo1 + 1;}
+        termo1 = termo1 + 1;
+        termo2 = 1;
+  }
        
      
   
 }
+
+
+
+
 
