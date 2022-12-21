af82cd466d77dd371a0b523ce0426b5e75ceec04
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-20 23:48:11 -0400

    Create 202222220005.c

---
+++ aula_3/202222220005.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int main() {
+  int cont = 0, num = 0, num1, menoval = 0;
+  while (cont < 20){
+      cont = cont + 1;
+      printf("Numeros inteiros: ");
+      scanf("%d", &num1);
+      
+      if (num1 < 0) {
+          printf("Não é permitida a entrada de números negativos");
+          return;
+      }
+      if (num1 > num){
+        num = num1;
+      }
+      if (cont==1){
+         menoval=num1;
+      }
+      if (num1 < menoval){
+        menoval = num1;  
+      }
+  }
+  
+printf("O maoir numero inteiro eh: %d\nO menor numero eh: %d", num, menoval);
+  
+}
+
