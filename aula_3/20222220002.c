fd77539663f60bbdc956a4c29eb4bc858395037e
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-21 11:36:56 -0300

    Update and rename 20221215_002.c to 20222220002.c

--- aula_3/20222220002.c
+++ aula_3/20222220002.c
@@ -1,8 +1,12 @@
+
+/*
+20222220002 Escreva um algoritmo que lê 50 números inteiros e em seguida mostra a soma de todos os ímpares lidos.
+*/
 #include <stdio.h>
 
 int main() {
   int cont = 0, num = 0, num1;
-  while (cont < 5){
+  while (cont < 50){
       cont = cont + 1;
       printf("Numeros para soma: ");
       scanf("%d", &num1);
