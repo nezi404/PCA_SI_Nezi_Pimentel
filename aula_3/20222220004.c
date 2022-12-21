33b77796ace9c290588dca7796bc7346e1bbb4f2
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-20 23:33:42 -0400

    Update 20222220004.c

--- aula_3/20222220004.c
+++ aula_3/20222220004.c
@@ -2,15 +2,19 @@
 #include <stdio.h>
 
 int main() {
-  int cont = 0, num = 0, num1, menoval;
+  int cont = 0, num = 0, num1, menoval = 0;
   while (cont < 5){
       cont = cont + 1;
       printf("Numeros inteiros: ");
       scanf("%d", &num1);
       if (num1 > num){
         num = num1;
+        
       }
-      if (num1 < num){
+      if (cont==1){
+         menoval=num1;
+      }
+      if (num1 < menoval){
         menoval = num1;  
       }
   }
@@ -18,3 +22,4 @@ int main() {
 printf("O maoir numero inteiro eh: %d\nO menor numero eh: %d", num, menoval);
   
 }
+
