88cf26a997b498429378ff2e4ca57b495192940a
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-20 23:05:34 -0400

    Update 20221215_001.c

--- aula_3/20221215_001.c
+++ aula_3/20221215_001.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
 int main() {
-  int cont = 0, num = 0, num1 ;
-  while (cont < 5){
-      cont = cont + 1;
-      printf("Numeros para soma: ");
-      scanf("%d", &num1);
-      num = num1 + num;}
-printf("a soma: %d", num);
-  
-}
+    int num, resul = 0;
+    printf("Número inteiro: ");
+    scanf("%d", &num);
+    resul = num + resul;  
+    printf("Número inteiro: ");
+    scanf("%d", &num);
+    resul = num + resul;
+    printf("Número inteiro: ");
+    scanf("%d", &num);
+    resul = num + resul;
+    printf("Número inteiro: ");
+    scanf("%d", &num);
+    resul = num + resul;
+    printf("Número inteiro: ");
+    scanf("%d", &num);
+    resul = num + resul;
+    printf("O resultado da soma eh: %d",resul);
 
-// twm que ter so uma variavel
+    return 0;
+}
