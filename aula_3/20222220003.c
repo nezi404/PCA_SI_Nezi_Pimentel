3e8c33b3924e4c1c63f34af2c2cc4d6a5f47fe3c
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-21 11:43:16 -0300

    Update and rename 202222220003.c to 20222220003.c

--- aula_3/20222220003.c
+++ aula_3/20222220003.c
@@ -1,12 +1,17 @@
+/*
+20222220003 Altere o algoritmo anterior para que ele considere apenas a soma dos ímpares que estejam entre 100 e 200.
+
+*/
+
 #include <stdio.h>
 
 int main() {
   int cont = 0, num = 0, num1;
-  while (cont < 5){
+  while (cont < 50){
       cont = cont + 1;
       printf("Numeros para soma: ");
       scanf("%d", &num1);
-      if (num1%2 !=0)
+      if (num1%2 !=0 && 100 < num1 && num1 < 200)
         num = num1 + num;
       
   }
