1a9f079998ddc4ca5c426c523a8fa3598e2f5138
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-24 12:15:06 -0300

    Update 20221222_005.c

--- aula_04/20221222_005.c
+++ aula_04/20221222_005.c
@@ -3,19 +3,27 @@
 soma: soma = 1/1 + 3/2 + 5/3 + 7/4 + ... + 99/50
 */
  
-TA ERRANDOOOOOOOOOOO
 #include <stdio.h>
 
 int main(){
-	float fracao= 0, somafra = 0;
-	int num = 1 , deno = 1;
-	
-	while (deno < 50){
+	float fracao= 0, somafra = 0, num = 1 , deno = 1;
+
+	while (deno <= 50){
 		fracao = num/deno;
-		printf("%01d / %01d\n", num, deno);
-		num = num + 2;
-		deno = deno + 1;
-		somafra = somafra + fracao;
+
+		if (deno == 50){
+			printf("%0.f/%0.f = ", num, deno);
+			deno++;
+			somafra = somafra + fracao;
+	
+	}
+		else{
+		
+			printf("%0.f/%0.f + ", num, deno);
+			num = num + 2;
+			deno = deno + 1;
+			somafra = somafra + fracao;
+	}
 	
 		
 		}
