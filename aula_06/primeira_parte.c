310b122491f5789e32cb14bb1377f5639c7e298b
nezi404 <105327627+nezi404@users.noreply.github.com>
2023-01-11 13:02:45 -0400

    Create primeira_parte.c

---
+++ aula_06/primeira_parte.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+    int pala, palalen ;
+    char palav[10];
+   
+    printf("número ");
+    scanf("%s", palav);
+    printf("%s", palav);
+    palalen = strlen(palav);
+    pala = atoi(palav);
+    printf("\n\n aa %d", palalen);
+
+    return 0;
+}
