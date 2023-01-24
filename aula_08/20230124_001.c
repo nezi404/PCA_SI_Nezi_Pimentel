bde65a86f135f7f35f9ad89a4be2a63b6bc5de67
nezi404 <105327627+nezi404@users.noreply.github.com>
2023-01-24 17:23:32 -0400

    Update 20230124_001.c

--- aula_08/20230124_001.c
+++ aula_08/20230124_001.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-	float seq[10];
+	float seq[100];
 	int cont ;
-	for (cont = 0; cont < 10; cont++){
+	for (cont = 0; cont < 100; cont++){
 		printf("Numero menor que 100:\n");
 		scanf("%f", &seq[cont]);
 		if (seq[cont] > 100
@@ -13,7 +13,7 @@ int main(){
 		}
 		
 	}
-	for (cont = 9; cont >= 0; cont--){
+	for (cont = 99; cont >= 0; cont--){
 	
 	printf("%0.3f\n", seq[cont]);
 	}
