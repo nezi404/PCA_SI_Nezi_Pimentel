5fc28b26fc0f1386fd137b4dbec59821f17d9212
nezi404 <105327627+nezi404@users.noreply.github.com>
2023-01-27 09:48:33 -0300

    Update and rename 20230126_002 to 20230126_002.c

--- aula_09/20230126_002.c
+++ aula_09/20230126_002.c
@@ -3,21 +3,20 @@
 int matrix(int numlin, int numcol){
     
 int mat1[numlin][numcol],  mat2[numlin][numcol] , mat3[numlin] [numcol];
-	int cont1, cont2, len = sizeof (mat1) / sizeof (mat1[0]);
+	int cont1, cont2;
 	
 	
 	    for(cont1 = 0; cont1 < numlin ; cont1++){
 		for (cont2 = 0; cont2  < numcol; cont2++){
 			printf("NUmeros da matriz 1 linha %d coluna %d: ", cont1 +1, cont2+ 1);
-			printf("%d", len);
-			scanf("%d", mat1[cont1][cont2]);
+			scanf("%d", &mat1[cont1][cont2]);
 			
 		}
 	}
 	      for(cont1 = 0; cont1 < numlin ; cont1++){
 		for (cont2 = 0; cont2  < numcol; cont2++){
 			printf("NUmeros da matriz 2 linha %d coluna %d: ", cont1 + 1, cont2+ 1);
-			scanf("%d", mat2[cont1][cont2]);
+			scanf("%d", &mat2[cont1][cont2]);
 			
 		}
 	}
@@ -31,7 +30,7 @@ int mat1[numlin][numcol],  mat2[numlin][numcol] , mat3[numlin] [numcol];
 
 		for(cont1 = 0; cont1 < numlin; cont1++){
 		for (cont2 = 0; cont2  < numcol; cont2++){
-			printf("%d ", mat3[cont1][cont2]);
+			printf("\t%d\t", mat3[cont1][cont2]);
 			
 			
 		}
@@ -43,7 +42,7 @@ int main(){
     int numlin_, numcol_;
 	printf("Qual o numero de linhas? ");
     scanf("%d", &numlin_);
-    printf("Qual o numero de coluna? ");
+    printf("Qual o numero de colunas? ");
     scanf("%d", &numcol_);
     matrix(numlin_, numcol_);
 
