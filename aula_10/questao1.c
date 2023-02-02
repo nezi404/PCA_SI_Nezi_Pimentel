b02d4791df3a1c0de39724d0afe90f7d66028289
nezi404 <105327627+nezi404@users.noreply.github.com>
2023-02-02 18:04:29 -0300

    Update questao1.c

--- aula_10/questao1.c
+++ aula_10/questao1.c
@@ -12,14 +12,34 @@ void semvogal(char semv[100]){
         printf("%s", semv[cont1]);
 
         
+
+        
     } 
 }
+void letmaiu(char maiu[100]){
+    int cont1, tam;
+    printf("%s", strupr(maiu));
+    
+}
 
+void numlet(char pala[100]){
+    int cont1, tam, numero;
+    char num[]= {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z};
+    tam = strlen(pala[100]) + 1;
+    for (cont1 = 0; cont1 < tam; cont1++){
+        numero = strspn(pala[cont1], num[]);
+        printf("%s", numero);
+
+        
+
+        
+    } 
+}
 int main(){
     char palavra[] = " ";
     printf("Insira a palavra: ");
     scanf("%s", palavra);
     rever(palavra);
-    semvogal(palavra);
-    
+    letmaiu(palavra);
+    numlet(palavra);
 }
