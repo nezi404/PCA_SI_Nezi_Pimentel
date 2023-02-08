3f56cffdb7755ecd86b719601b1e57e93cb745d3
nezi404 <105327627+nezi404@users.noreply.github.com>
2023-02-08 01:25:40 -0400

    Update 20230202_001.c

--- aula_11/20230202_001.c
+++ aula_11/20230202_001.c
@@ -1,3 +1,4 @@
+
 /*20230202_001 Na turma existem 5 equipes, cada equipe está integrada por 4 alunos
  definidos pelo nome e sobrenome do aluno.  Crie um programa que permita armazenar os integrantes 
  das equipes e incorpore a funcionalidade que dado um nome retorne em a equipe do aluno.
@@ -7,10 +8,10 @@
 #include <string.h>
 
 int main(){
-	char nome[100], inserir[] = "s", esta[80], equi1[4][8], equi2[4][8], equi3[4][8], equi4[4][8], equi5[4][8];
-	int equi = 0, comp =0, cont1 = 0, cont2 = 0, cont3 = 0, cont4 = 0, cont5 = 0, ass = 100, achar = 0, achaeq = 0;
+	char nome[100], inserir[] = "s", equi1[8][15], equi2[8][15], equi3[8][15], equi4[8][15], equi5[8][15];
+	int equi = 0, comp =0, cont1 = 0, cont2 = 0, cont3 = 0, cont4 = 0, cont5 = 0, ass = 10, achaeq = 0;
 	
-	while (comp < 10){
+	while (comp < 20){
 		printf("Equipe do aluno: ");
 		scanf("%d", &equi);
 		if (ass != 0){
@@ -18,9 +19,9 @@ int main(){
 		}
 		ass = 0;
 
-		if (equi == 1 ){
-			if (cont1 < 4){
-							printf("aaaaa %d aaaaa", equi);
+		if (equi == 1 && ass == 0){
+			if (cont1 < 8){
+							
 
 			
 			scanf("%s %s", equi1[cont1], equi1[cont1 + 1]);
@@ -30,9 +31,9 @@ int main(){
 			}
 
 		}
-		if (equi == 2){
-			printf("aaaaa %d aaaaa", equi);
-			if (cont2 < 4){
+		if (equi == 2 && ass == 0){
+			
+			if (cont2 < 8){
 			
 			scanf("%s %s", equi2[cont2], equi2[cont2+1]);
 			cont2+=2;
@@ -40,9 +41,9 @@ int main(){
 			ass = 1;
 		}
 		}
-		if (equi == 3 ){
-			if (cont3 < 4){
-						printf("aaaaa %d aaaaa", equi);
+		if (equi == 3 && ass == 0){
+			if (cont3 < 8){
+						
 
 			scanf("%s %s", equi3[cont3], equi3[cont3+1]);
 			cont3+=2;
@@ -51,9 +52,9 @@ int main(){
 		}
 			
 		}
-		if (equi == 4){
-			if (cont4 < 4){
-						printf("aaaaa %d aaaaa", equi);
+		if (equi == 4 && ass == 0){
+			if (cont4 < 8){
+						
 
 			scanf("%s %s", equi4[cont4], equi4[cont4+1]);
 			cont4+=2;
@@ -61,9 +62,9 @@ int main(){
 			ass = 1;
 			}
 		}
-		if (equi == 5 ){
-			if (cont5 < 4){
-							printf("aaaaa %d aaaaa", equi);
+		if (equi == 5 && ass == 0){
+			if (cont5 < 8){
+							
 
 			
 			scanf("%s %s", equi5[cont5], equi5[cont5+1]);
@@ -87,31 +88,26 @@ int main(){
 		scanf("%s", nome);
 		while (cont1 < 8 ){
 			
-			if (strcmp(nome, equi1[cont1]) == 0){
-				//esta = equi1[cont1];
+			if (strcmp(nome, equi1[cont1]) == 0 && achaeq == 0){
 				achaeq = 1;	 
 				printf("o aluno  esta na equipe %d\n", achaeq);
 			}
-				if (strcmp(nome, equi2[cont1]) == 0){
-				//esta[1] = equi2[cont1];
+				if (strcmp(nome, equi2[cont1]) == 0 && achaeq == 0){
 				achaeq = 2;	 
 				printf("o aluno  esta na equipe %d\n", achaeq);
 
 			}
-			if (strcmp(nome, equi3[cont1]) == 0){
-				//esta[1] = equi3[cont1];
+			if (strcmp(nome, equi3[cont1]) == 0 && achaeq == 0){
 				achaeq = 3;	 
 				printf("o aluno  esta na equipe %d\n", achaeq);
 
 			}
-			if (strcmp(nome, equi4[cont1]) == 0){
-				//esta[1] = equi4[cont1];
+			if (strcmp(nome, equi4[cont1]) == 0 && achaeq == 0){
 				achaeq = 4;	 
 				printf("o aluno  esta na equipe %d\n", achaeq);
 				
 			}
-				if (strcmp(nome, equi5[cont1]) == 0){
-				//esta[1] = equi5[cont1];
+				if (strcmp(nome, equi5[cont1]) == 0 && achaeq == 0){
 				achaeq = 5 ;
 				printf("o aluno  esta na equipe %d\n", achaeq);	 
 			}
@@ -126,16 +122,7 @@ int main(){
 		printf("Deseja inserir outro nome?s/n \n");
 		scanf("%s", inserir);
 		
-		}		cont1=0;
-		cont2=0;
-		while (cont1 < 20){
-			printf("\n aluno 1 : %s\n", equi2[cont1]);
-			cont1++;
-		}
+		}	
 		
-		
-	
-	
-
 
 }
