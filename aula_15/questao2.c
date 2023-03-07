a4308e4094d8ab8e3d41df2d8b6df419be7346ba
nezi404 <105327627+nezi404@users.noreply.github.com>
2023-03-07 17:56:28 -0400

    Create questao2.c

---
+++ aula_15/questao2.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int achar(int *pontei, int *pontef, int *ponteiron, *ponteiroatual){
+    if (*ponteiron == *ponteiroatual)
+}
+int main() {
+    int vetor[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int *pnumd, *pnumatu, i;
+    int numerodois = 2;
+    int pini = &vetor[0];
+    int pfin = &vetor[9];
+    *pnumd = &numerodois;
+    for (i = 0; i < pfin; i++){
+        *pnumatua = &vetor[i];
+        achar(pini, pfin, pnumd, )
+    }
+
+}
