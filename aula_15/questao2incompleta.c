37485944fb603c5d18318a4a544ae958b8a7485c
nezi404 <105327627+nezi404@users.noreply.github.com>
2023-03-14 17:53:14 -0400

    Create questao2incompleta.c

---
+++ aula_15/questao2incompleta.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int *achar( int *ponteiron, int *ponteiroatual){
+    if (*ponteiron == *ponteiroatual ){
+        
+        //printf("O ponteiro do valor %d eh %d\n", *ponteiroatual, ponteiroatual);
+        return ponteiroatual;
+    }
+    if (*ponteiron != *ponteiroatual){
+        return NULL;
+        
+    }
+}
+int main() {
+    int vetor[15] = {1, 2, 3, 4, 2, 5, 6, 2, 7, 8, 9, 10, 11, 12};
+    int i, numeroon = 0, naotem= 0, *pnumeroon = &naotem, *ver;
+    int numerodois = 2;
+    int *pini = &vetor[0];
+    int *pfin = &vetor[14];
+    int *pnumd = &numerodois;
+    int *pnumatua = &vetor[0] ;
+    int *pvet;
+    
+    //incompleta questao 2 pater a)
+    
+    pvet = pini; 
+    while(*pvet != NULL){
+        pvet++;
+        pnumatua = pvet;
+        ver = achar(pnumd, pvet);
+        if (ver == NULL){
+            printf("Nao encontrado na lista\n");
+        }
+        else{
+            ver++;
+            while(ver != NULL){
+                ver = achar(ver, pvet);
+                if(ver == NULL){
+                    printf("Nao encontrado\n");
+                }
+                else{
+                    printf("O valor %d eh %d\n", *ver, ver);
+                }
+            }
+        }
+    }
+
+}
