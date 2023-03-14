5ea064b83bc144507800c33e61584364c3d4ca41
nezi404 <105327627+nezi404@users.noreply.github.com>
2023-03-14 17:16:18 -0400

    Update 20230307_002.c

--- aula_15/20230307_002.c
+++ aula_15/20230307_002.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-int achar(int *pontei, int *pontef, int *ponteiron, int *ponteiroatual){
+int achar( int *ponteiron, int *ponteiroatual){
     if (*ponteiron == *ponteiroatual ){
         
-        printf("O ponteiro do valor %d eh %d\n", *ponteiroatual, ponteiroatual);
+        //printf("O ponteiro do valor %d eh %d\n", *ponteiroatual, ponteiroatual);
+        return ponteiroatual;
     }
     if (*ponteiron != *ponteiroatual){
-        int nullvalue = 0;
-        ponteiroatual = &nullvalue;
-        printf("O ponteiro do valor eh nulo %d : %d\n", ponteiroatual, *ponteiroatual);
+        return NULL;
         
     }
 }
 int main() {
     int vetor[15] = {1, 2, 3, 4, 2, 5, 6, 2, 7, 8, 9, 10, 11, 12};
-    int i, numeroon = 0, naotem= 0, *pnumeroon = &naotem;
+    int i, numeroon = 0, naotem= 0, *pnumeroon = &naotem, *ver;
     int numerodois = 2;
     int *pini = &vetor[0];
     int *pfin = &vetor[14];
@@ -24,7 +23,13 @@ int main() {
     
     for (pvet = pini; pnumatua < pfin; pvet++){
         pnumatua = pvet;
-        achar(pini, pfin, pnumd, pvet);
+        ver = achar(pnumd, pvet);
+        if (ver == NULL){
+            printf("Nao encontrado na lista\n");
+        }
+        else{
+            printf("O valor %d eh %d\n", numerodois, ver);
+        }
     }
 
 }
