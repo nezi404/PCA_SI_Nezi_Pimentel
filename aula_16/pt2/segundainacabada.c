bb2b019f829f26d90c2edb38a734b9dad88b7e1d
nezi404 <105327627+nezi404@users.noreply.github.com>
2023-03-18 23:31:08 -0400

    Update segundainacabada.c

--- aula_16/pt2/segundainacabada.c
+++ aula_16/pt2/segundainacabada.c
@@ -1,45 +1,47 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 #include <stdlib.h>
 
     
 int main() {
-    int *mat, taml, tamc, i,j, num;
+    int **mat, taml, tamc, i,j, num;
     printf("Qual o valor de linhas e colunas?");
     scanf(" %d %d", &taml, &tamc);
-    mat = (int *)malloc(taml*tamc*sizeof(int));
-    if (mat!= NULL){
-        printf("foi\n");
+    mat = malloc(taml * sizeof(int*));
+     for (i=0;i<taml;i++){
+           mat[i] = malloc(tamc * sizeof(int));
+        }
+    
         
         for (i=0;i<taml;i++){
             for (j=0; j<tamc;j++){
                 printf("Numeros da coluna %d e linha %d: ", j+1, i+1 );
                 scanf("%d", &num);
-                *(mat + tamc*i + j) = num; 
+                *(*(mat + i) + j) = num; 
             }
         }
+        printf("\n imprimir por aritmetica de ponteiro\n");
         for (i=0;i<taml;i++){
             for (j=0; j<tamc;j++){
-                printf(" %d ", *(mat + tamc*i + j)); 
+                printf(" %d ", *(*(mat + i) + j)); 
+            }
+            printf("\n");
+            
+        }
+        printf("\n imprimir por index\n");
+        for (i=0;i<taml;i++){
+            for (j=0; j<tamc;j++){
+                printf(" %d ", *(*(mat + i) + j)); 
             }
             printf("\n");
             
         }
         
+        
         for (i=0;i<taml;i++){
-            for (j=0; j<tamc;j++){
-                printf(" %d ", *mat[i]); 
-            }
-            printf("\n");
-            
+                free(mat[i]);
+     
         }
         free(mat);
-        
-        
-    }
-    if (mat == NULL){
-        printf("nao foi");
-    }
-    
+
     
 }
